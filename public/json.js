d4d989ad99803d76353a8e3cf35b0775ac4aeb2e
omar lopez <omarlaszloo@gmail.com>
2014-11-19 15:38:41 -0600

    se agrega proyecto http_server

---
+++ public/json.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+	".html":"text/html",
+	".css" :"text/css",
+	".js"  :"aplication/javascript",
+	".png" :"imagenes/png",
+	".gif" :"imagenes/gif",
+	".jpg" :"imagenes/jpeg",
+	".jpeg" :"imagenes/jpeg",
+
+}
